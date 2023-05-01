249f35c371a02385e76418bbca42cb8e89ee268a
Gasan Nuriev <C:\Users\Gasan\AppData\Roaming\The Bat!>
2023-05-01 12:11:55 +0300

    commited tack 30 end 32

---
+++ tack32.py
@@ -0,0 +1,19 @@
+# Задача 32: Определить индексы элементов массива (списка), значения которых принадлежат
+# заданному диапазону (т.е. не меньше заданного минимума и не больше заданного максимума)
+
+# n = int(input('promt n:'))
+# min_num = 0
+# for i in range(n):
+#     if min_num <= i:
+#         min_num = i
+#         print(f'min number {min_num} ')
+#     # else:
+#     #     min_mum = +i
+# # max_elem = 0
+# # for i in range (n):
+# #     if max_elem > i:
+numbers = [1, 5, 6, 8, 9, 45]  # int(input('Input numbers from comma:  '))
+min_number = min(numbers)
+print(min_number)
+max_number = max(numbers)
+print(max_number)
